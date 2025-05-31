df8e6c3e5dc7f396542edc40bfea89744b2ec0c0
masuddall336 <abdullahalll336@gmail.com>
2025-05-31 09:48:43 +0600

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-console.log(10);
-let takeUserid = document.getElementById('userName');
-let password = document.getElementById('password');
-let takeLoginButton = document.getElementById('loginButton');
-takeLoginButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    if (takeUserid.value === "masudalll336@gmail.com" && password.value === "123645") {
-        alert('Right');
-        location.href = "inside.html";
-    } else {
+// console.log(10);
+// let takeUserid = document.getElementById('userName');
+// let password = document.getElementById('password');
+// let takeLoginButton = document.getElementById('loginButton');
+// takeLoginButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     if (takeUserid.value === "masudalll336@gmail.com" && password.value === "123645") {
+//         alert('Right');
+//         location.href = "inside.html";
+//     } else {
 
-        alert('Wrong!')
+//         alert('Wrong!')
+//     }
+
+
+// })
+
+let takeAdd = document.getElementById('add');
+console.log(takeAdd);
+takeAdd.addEventListener('click', function () {
+    console.log('clicked');
+    console.log(takeAdd.classList);
+    if (takeAdd.classList.includs === "single_card_wrapper") {
+        console.log("Right");
+
+    }else{
+        console.log("False");
+        
     }
-
 
 })
